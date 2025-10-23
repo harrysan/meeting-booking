058a587d847be8c974fb5bfe162125fe3c94911b
harrysan <harrysantoso8@yahoo.com>
2025-10-23 14:41:00 +0700

    Change Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meeting Booking System</title>
+    <title>Meeting Booking System SD6</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .table-responsive table {
